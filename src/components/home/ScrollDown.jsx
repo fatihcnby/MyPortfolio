8a85661443785f684de2961a5afbe0157981691b
fatihcnby <fatihcnby@gmail.com>
2024-04-05 14:13:01 +0300

    changed

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -7,7 +7,7 @@ const ScrollDown = () => {
         <svg
           width="32px"
           height="32px"
-          class="home__scroll-mouse"
+          className="home__scroll-mouse"
           viewBox="0 0 247 390"
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
